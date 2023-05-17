e79dc923ad48c928da788cce1f5cb9235dc5243c
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-05-17 22:01:54 +0700

    add partner view

---
+++ resources/views/dashboard/partner/index.blade.php
@@ -0,0 +1,51 @@
+@extends('dashboard.layouts.main')
+@section('title', "Partner")
+@section('content')
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        {{-- <h1 class="h3 mb-2 text-gray-800">Partner List</h1> --}}
+
+        <!-- DataTales Example -->
+        <div class="card border-0 shadow mb-4">
+            <div class="card-header py-3">
+                <div class="btn btn-primary btn-sm px-4 border-0 shadow-sm">Add</div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Email</th>
+                                <th>Username</th>
+                                <th>Count Order</th>
+                                <th>Admin Id</th>
+                                <th>Account Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                use Faker\Factory as Faker;
+
+                                $faker = Faker::create('id_ID');
+                                for ($i=1; $i <= 100; $i++) { 
+                                    $randomAdminId = random_int(1, 5);
+                                    $randomCountOrder = random_int(50, 200);
+                                    $operationalStatus=array("Open","Close");
+                                    echo "<tr>";
+                                        echo "<td>$faker->email</td>";
+                                        echo "<td>$faker->username</td>";
+                                        echo "<td>$randomCountOrder</td>";
+                                        echo "<td>$randomAdminId</td>";
+                                        echo '<td>'.$operationalStatus[array_rand($operationalStatus)].'</td>';
+                                    echo "</tr>";
+                                }
+                            @endphp
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::prefix('dashboard')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/table', [DashboardController::class, 'table'])->name('tabledashboard');
@@ -29,4 +18,10 @@ Route::prefix('dashboard')->group(function () {
     Route::get('/login', [DashboardController::class, 'login'])->name('dashboardlogin');
     Route::get('/register', [DashboardController::class, 'register'])->name('dashboardregister');
     Route::get('/forgot-password', [DashboardController::class, 'forgot_password'])->name('dashboardforgorpassword');
+
+    // Route::get('partner', function () {
+    //     return view('dashboard.partner.index');
+    // })->name('partner.index');
+
+    Route::view('/partner', 'dashboard.partner.index')->name('dashboard.partner.index');
 });
