cf33bea2958d57cb45ed7cf7ce7187a60ce2b253
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-05-27 08:01:33 +0700

    add FE cms for partner

--- resources/views/dashboard/partner/index.blade.php
+++ resources/views/dashboard/partner/index.blade.php
@@ -1,51 +1,88 @@
 @extends('dashboard.layouts.main')
-@section('title', "Partner")
+@section('title', 'Partner')
 @section('content')
-    <div class="container-fluid">
+    <div class="container px-3">
+        @php
+            use Faker\Factory as Faker;
+            
+            $faker = Faker::create('id_ID');
+            $partners = [];
+            
+            for ($i = 1; $i <= 10; $i++) {
+                $randomAdminId = random_int(1, 5);
+                $randomCountOrder = random_int(50, 200);
+                $accountStatus = ['Confirmed', 'Unconfirmed'];
+            
+                $username = $faker->username; // Generate a username
+                $email = $faker->email; // Generate an email
+            
+                $partners[] = [
+                    'username' => $username,
+                    'email' => $email,
+                    'randomCountOrder' => $randomCountOrder,
+                    'randomAdminId' => $randomAdminId,
+                    'accountStatus' => $accountStatus[array_rand($accountStatus)],
+                ];
+            }
+        @endphp
 
-        <!-- Page Heading -->
-        {{-- <h1 class="h3 mb-2 text-gray-800">Partner List</h1> --}}
-
-        <!-- DataTales Example -->
-        <div class="card border-0 shadow mb-4">
-            <div class="card-header py-3">
-                <div class="btn btn-primary btn-sm px-4 border-0 shadow-sm">Add</div>
+        <!-- Search -->
+        <div class="input-group mb-4 d-flex justify-content-end">
+            <div class="form-outline">
+                <input type="search" id="searchInput" class="form-control" placeholder="Search" />
             </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered" id="dataTable" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th>Email</th>
-                                <th>Username</th>
-                                <th>Count Order</th>
-                                <th>Admin Id</th>
-                                <th>Account Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php
-                                use Faker\Factory as Faker;
-
-                                $faker = Faker::create('id_ID');
-                                for ($i=1; $i <= 100; $i++) { 
-                                    $randomAdminId = random_int(1, 5);
-                                    $randomCountOrder = random_int(50, 200);
-                                    $operationalStatus=array("Open","Close");
-                                    echo "<tr>";
-                                        echo "<td>$faker->email</td>";
-                                        echo "<td>$faker->username</td>";
-                                        echo "<td>$randomCountOrder</td>";
-                                        echo "<td>$randomAdminId</td>";
-                                        echo '<td>'.$operationalStatus[array_rand($operationalStatus)].'</td>';
-                                    echo "</tr>";
-                                }
-                            @endphp
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+            <button type="button" class="btn btn-primary">
+                <i class="fas fa-search"></i>
+            </button>
         </div>
 
+        <div class="px-3 d-flex gap-3 justify-content-center flex-wrap" id="partnerCards">
+            @foreach ($partners as $partner)
+                <div class="card border-0 shadow-sm rounded col-xl-2 text-center p-3">
+                    <div class="text-center my-2">
+                        <img src="{{ asset('assets/dashboard/img/dummyimage.png') }}" class="rounded-circle"
+                            style="width: 60%" alt="Avatar" />
+                    </div>
+                    <p class="my-1">
+                        @if ($partner['accountStatus'] == 'Confirmed')
+                            <a href="#"
+                                class="badge px-2 py-1 text-white bg-success">{{ $partner['accountStatus'] }}</a>
+                        @else
+                            <a href="#"
+                                class="badge px-2 py-1 text-white bg-danger">{{ $partner['accountStatus'] }}</a>
+                        @endif
+                    </p>
+                    <p class="mb-2">{{ $partner['username'] }}</p>
+                    <div class="d-flex justify-content-center gap-2">
+                        <div class="btn btn-sm btn-primary btn-icon shadow-sm"><i class="fa-solid fa-circle-info"></i></div>
+                        <div class="btn btn-sm btn-warning btn-icon shadow-sm"><i class="fa-solid fa-pen-to-square"></i>
+                        </div>
+                        <div class="btn btn-sm btn-danger btn-icon shadow-sm"><i class="fa-solid fa-trash"></i></div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
+
+    <script>
+        // Get the search input element
+        const searchInput = document.getElementById('searchInput');
+
+        // Add an event listener for the 'keyup' event
+        searchInput.addEventListener('keyup', function() {
+            const searchValue = searchInput.value.toLowerCase();
+            const cards = document.querySelectorAll('.card');
+
+            cards.forEach(function(card) {
+                const username = card.querySelector('.mb-2').textContent.toLowerCase();
+
+                // Show or hide the card based on the search value
+                if (username.includes(searchValue)) {
+                    card.style.display = 'block';
+                } else {
+                    card.style.display = 'none';
+                }
+            });
+        });
+    </script>
 @endsection
