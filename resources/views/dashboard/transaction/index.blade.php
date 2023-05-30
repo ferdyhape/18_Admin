833205e843fd2a3ef6300bf72225fe4251f5a9cd
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-05-30 23:34:21 +0700

    add create modal for transaction and change detail button

--- resources/views/dashboard/transaction/index.blade.php
+++ resources/views/dashboard/transaction/index.blade.php
@@ -8,7 +8,53 @@
             <div class="card-header">
                 <div class="d-flex justify-content-between py-auto">
                     <p class="my-auto">Transaction List</p>
-                    <div class="btn btn-primary btn-sm px-4 border-0 shadow-sm">Add</div>
+                    <div class="btn btn-primary btn-sm px-4 border-0 shadow-sm" data-toggle="modal"
+                        data-target="#modalCreateTransaction">Add</div>
+                </div>
+                <div class="modal fade" id="modalCreateTransaction" tabindex="-1" role="dialog"
+                    aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content border-0">
+                            <div class="modal-header text-center">
+                                <h4 class="modal-title w-100 font-weight-bold">New Transaction</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form action="">
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <input class="form-control mb-3 @error('quantity') is-invalid @enderror"
+                                            type="number" name="quantity" placeholder="Quantity">
+                                        @error('quantity')
+                                            <div class="form-text">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="form-control mb-3 @error('sub_price') is-invalid @enderror"
+                                            type="number" name="sub_price" placeholder="Sub Price">
+                                        @error('sub_price')
+                                            <div class="form-text">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="form-control mb-3 @error('price') is-invalid @enderror" type="number"
+                                            name="price" placeholder="Price" disabled>
+                                        @error('price')
+                                            <div class="form-text">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary btn-sm shadow-sm" data-dismiss="modal"
+                                        aria-label="Close">
+                                        <span aria-hidden="true">Cancel</span>
+                                    </button>
+                                    <button type="submit" class="btn btn-primary btn-sm shadow-sm">Create</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="card-body">
@@ -50,7 +96,8 @@
                                     <td>@toRP($transaction['subPrice'])</td>
                                     <td>@toRP($transaction['price'])</td>
                                     <td class="text-center">
-                                        <div class="btn btn-sm btn-primary btn-icon shadow-sm">Detail</div>
+                                        <div class="btn btn-sm btn-primary btn-icon shadow-sm"><i
+                                                class="fa-solid fa-download"></i> Download</div>
                                     </td>
                                 </tr>
                             @endforeach
@@ -59,6 +106,29 @@
                 </div>
             </div>
         </div>
-
     </div>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var quantityInput = document.querySelector('input[name="quantity"]');
+            var subPriceInput = document.querySelector('input[name="sub_price"]');
+            var priceInput = document.querySelector('input[name="price"]');
+
+            quantityInput.addEventListener('input', calculateValuePrice);
+            subPriceInput.addEventListener('input', calculateValuePrice);
+
+            function calculateValuePrice() {
+                var quantity = parseFloat(quantityInput.value);
+                var subPrice = parseFloat(subPriceInput.value);
+
+                if (!isNaN(quantity) && !isNaN(subPrice)) {
+                    var valuePrice = quantity * subPrice;
+                    // Convert the float value to an integer
+                    priceInput.value = parseInt(valuePrice);
+                } else {
+                    priceInput.value = '';
+                }
+            }
+        });
+    </script>
 @endsection
