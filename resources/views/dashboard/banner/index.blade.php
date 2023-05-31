958f4d2ee03de07fbcb0eb646821a39c2394973d
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-05-31 20:51:02 +0700

    update index of banner

--- resources/views/dashboard/banner/index.blade.php
+++ resources/views/dashboard/banner/index.blade.php
@@ -9,18 +9,66 @@
         <!-- DataTales Example -->
         <div class="card border-0 shadow mb-4">
             <div class="card-header py-3">
-                <div class="btn btn-primary btn-sm px-4 border-0 shadow-sm">Add</div>
+                <div class="d-flex justify-content-between py-auto">
+                    <p class="my-auto">Banner List</p>
+                    <div class="btn btn-primary btn-sm px-4 border-0 shadow-sm" data-toggle="modal"
+                        data-target="#modalCreateBanner">Add</div>
+                </div>
+                <div class="modal fade" id="modalCreateBanner" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content border-0">
+                            <div class="modal-header text-center">
+                                <h4 class="modal-title w-100 font-weight-bold">New Banner</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form action="">
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <input class="form-control mb-3 @error('quantity') is-invalid @enderror"
+                                            type="number" name="quantity" placeholder="Quantity">
+                                        @error('quantity')
+                                            <div class="form-text">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="form-control mb-3 @error('sub_price') is-invalid @enderror"
+                                            type="number" name="sub_price" placeholder="Sub Price">
+                                        @error('sub_price')
+                                            <div class="form-text">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="form-control mb-3 @error('price') is-invalid @enderror" type="number"
+                                            name="price" placeholder="Price" disabled>
+                                        @error('price')
+                                            <div class="form-text">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary btn-sm shadow-sm" data-dismiss="modal"
+                                        aria-label="Close">
+                                        <span aria-hidden="true">Cancel</span>
+                                    </button>
+                                    <button type="submit" class="btn btn-primary btn-sm shadow-sm">Create</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
+
             <div class="card-body">
                 <div class="table-responsive">
                     <table class="table table-bordered" id="dataTable" cellspacing="0">
                         <thead>
                             <tr>
-                                <th>Email</th>
-                                <th>Username</th>
-                                <th>Count Order</th>
-                                <th>Admin Id</th>
-                                <th>Account Status</th>
+                                <th style="width: 70%">Banner Image</th>
+                                <th>Created By Admin</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -28,19 +76,32 @@
                                 use Faker\Factory as Faker;
                                 
                                 $faker = Faker::create('id_ID');
-                                for ($i = 1; $i <= 100; $i++) {
-                                    $randomAdminId = random_int(1, 5);
-                                    $randomCountOrder = random_int(50, 200);
-                                    $operationalStatus = ['Open', 'Close'];
-                                    echo '<tr>';
-                                    echo "<td>$faker->email</td>";
-                                    echo "<td>$faker->username</td>";
-                                    echo "<td>$randomCountOrder</td>";
-                                    echo "<td>$randomAdminId</td>";
-                                    echo '<td>' . $operationalStatus[array_rand($operationalStatus)] . '</td>';
-                                    echo '</tr>';
+                                $banners = [];
+                                for ($i = 1; $i <= 5; $i++) {
+                                    $admin_name = ['Ferdy', 'Gupron'];
+                                
+                                    $banners[] = [
+                                        'admin_name' => $admin_name[array_rand($admin_name)],
+                                    ];
                                 }
                             @endphp
+                            @foreach ($banners as $banner)
+                                <tr>
+                                    <td class="text-center"><img
+                                            src="{{ asset('assets/dashboard/img/dummybanner.png') }}"style="width: 60%"
+                                            alt="Avatar" /></td>
+                                    <td>{{ $banner['admin_name'] }}</td>
+                                    <td class="text-center">
+                                        <div class="d-flex justify-content-center gap-2">
+                                            <div class="btn btn-sm btn-warning btn-icon shadow-sm"><i
+                                                    class="fa-solid fa-pen-to-square"></i>
+                                            </div>
+                                            <div class="btn btn-sm btn-danger btn-icon shadow-sm"><i
+                                                    class="fa-solid fa-trash"></i></div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
