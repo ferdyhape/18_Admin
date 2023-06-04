228128373a7e7ae10eadf19fdbdfe820aabd1418
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-05 06:38:59 +0700

    create crud for banner and squarefeed

--- resources/views/dashboard/squarefeed/index.blade.php
+++ resources/views/dashboard/squarefeed/index.blade.php
@@ -3,19 +3,16 @@
 @section('content')
     <div class="container-fluid">
 
-        <!-- Page Heading -->
-        {{-- <h1 class="h3 mb-2 text-gray-800">Partner List</h1> --}}
-
         <!-- DataTales Example -->
         <div class="card border-0 shadow mb-4">
             <div class="card-header py-3">
                 <div class="d-flex justify-content-between py-auto">
                     <p class="my-auto">SquareFeed List</p>
                     <div class="btn btn-primary btn-sm px-4 border-0 shadow-sm" data-toggle="modal"
-                        data-target="#modalCreateBanner">Add</div>
+                        data-target="#modalCreateSquareFeed">Add</div>
                 </div>
-                <div class="modal fade" id="modalCreateBanner" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-                    aria-hidden="true">
+                <div class="modal fade" id="modalCreateSquareFeed" tabindex="-1" role="dialog"
+                    aria-labelledby="myModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content border-0">
                             <div class="modal-header text-center">
@@ -24,27 +21,18 @@
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
-                            <form action="">
+                            <form action="{{ url('dashboard/squarefeed/create') }}" method="POST"
+                                enctype="multipart/form-data">
+                                @csrf
                                 <div class="modal-body">
-                                    <div class="form-group">
-                                        <input class="form-control mb-3 @error('quantity') is-invalid @enderror"
-                                            type="number" name="quantity" placeholder="Quantity">
-                                        @error('quantity')
-                                            <div class="form-text">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                    <div class="form-group">
-                                        <input class="form-control mb-3 @error('sub_price') is-invalid @enderror"
-                                            type="number" name="sub_price" placeholder="Sub Price">
-                                        @error('sub_price')
-                                            <div class="form-text">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                    <div class="form-group">
-                                        <input class="form-control mb-3 @error('price') is-invalid @enderror" type="number"
-                                            name="price" placeholder="Price" disabled>
-                                        @error('price')
-                                            <div class="form-text">{{ $message }}</div>
+                                    <div class="input-group">
+                                        <label class="input-group-text" for="img_path">SquareFeed Image</label>
+                                        <input type="file" class="form-control @error('img_path') is-invalid @enderror"
+                                            id="img_path" name="img_path">
+                                        @error('img_path')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
                                         @enderror
                                     </div>
                                 </div>
@@ -72,32 +60,30 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @php
-                                use Faker\Factory as Faker;
-                                
-                                $faker = Faker::create('id_ID');
-                                $banners = [];
-                                for ($i = 1; $i <= 5; $i++) {
-                                    $admin_name = ['Ferdy', 'Gupron'];
-                                
-                                    $banners[] = [
-                                        'admin_name' => $admin_name[array_rand($admin_name)],
-                                    ];
-                                }
-                            @endphp
-                            @foreach ($banners as $banner)
+                            @foreach ($squarefeeds as $squarefeed)
                                 <tr>
                                     <td class="text-center"><img
-                                            src="{{ asset('assets/dashboard/img/dummysquarefeed.png') }}"style="width: 50%"
-                                            alt="Avatar" /></td>
-                                    <td>{{ $banner['admin_name'] }}</td>
+                                            src="{{ asset($squarefeed['img_path']) }}"style="width: 60%"
+                                            alt="SquareFeed-Image" /></td>
+                                    <td>{{ $squarefeed['admin']['username'] }}</td>
                                     <td class="text-center">
                                         <div class="d-flex justify-content-center gap-2">
-                                            <div class="btn btn-sm btn-warning btn-icon shadow-sm"><i
-                                                    class="fa-solid fa-pen-to-square"></i>
+                                            <div data-toggle="modal" data-target="#modalEditSquareFeed"
+                                                onclick="squareFeedModalEdit('{{ json_encode($squarefeed) }}')"
+                                                class="btn btn-sm btn-warning btn-icon shadow-sm">
+                                                <i class="fa-solid fa-pen-to-square"></i>
                                             </div>
-                                            <div class="btn btn-sm btn-danger btn-icon shadow-sm"><i
-                                                    class="fa-solid fa-trash"></i></div>
+                                            <button class="btn btn-sm btn-danger btn-icon shadow-sm"
+                                                id="delete-btn-{{ $squarefeed['id'] }}"><i
+                                                    class="fa-solid fa-trash"></i></button>
+
+                                            <form id="delete-form-{{ $squarefeed['id'] }}"
+                                                action="{{ url('dashboard/squarefeed') }}" method="POST"
+                                                style="display: none;">
+                                                @csrf
+                                                <input type="hidden" name="id" value="{{ $squarefeed['id'] }}"
+                                                    id="inputIdDeletePartner">
+                                            </form>
                                         </div>
                                     </td>
                                 </tr>
@@ -107,6 +93,76 @@
                 </div>
             </div>
         </div>
-
     </div>
+
+    <div class="modal fade" id="modalEditSquareFeed" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content border-0">
+                <div class="modal-header text-center">
+                    <h4 class="modal-title w-100 font-weight-bold">Edit SquareFeed</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form method="post" id="formEditSquareFeed" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="input-group">
+                            <label class="input-group-text" for="img_path">SquareFeed</label>
+                            <input type="file" class="form-control @error('img_path') is-invalid @enderror"
+                                id="img_path" name="img_path">
+                            @error('img_path')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary btn-sm shadow-sm" data-dismiss="modal"
+                            aria-label="Close">
+                            <span aria-hidden="true">Cancel</span>
+                        </button>
+                        <button type="submit" class="btn btn-primary btn-sm shadow-sm">Edit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script>
+        // edit
+        function squareFeedModalEdit(squarefeedJson) {
+            const squarefeed = JSON.parse(squarefeedJson);
+            document.getElementById('formEditSquareFeed').action = `squarefeed/${squarefeed.id}`;
+        }
+
+        // delete
+        const deleteButtons = document.querySelectorAll('button[id^="delete-btn"]');
+        deleteButtons.forEach(button => {
+            button.addEventListener('click', e => {
+                e.preventDefault();
+                console.log("Hello");
+
+                // Extract the ID from the button ID
+                const id = button.id.replace('delete-btn-', '');
+
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be delete this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // Submit the corresponding delete form
+                        const deleteForm = document.getElementById(`delete-form-${id}`);
+                        deleteForm.submit();
+                    }
+                })
+            });
+        });
+    </script>
 @endsection
