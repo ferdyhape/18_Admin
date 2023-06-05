8e78e1ab7d5b56d7c21f70382e4c3dcaba134466
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-06 03:20:15 +0700

    add defaultvariable middleware to get admin info

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'defaultVariable' => \App\Http\Middleware\DefaultVariableMiddleware::class,
     ];
 }
