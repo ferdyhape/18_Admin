90f7f918ca97667a69b9c509bb8a74759df1cdfb
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-05 22:42:34 +0700

    fixing bug for image size

--- resources/views/dashboard/partner/index.blade.php
+++ resources/views/dashboard/partner/index.blade.php
@@ -16,14 +16,8 @@
             @foreach ($partners as $partner)
                 <div class="card border-0 shadow-sm rounded col-xl-2 text-center p-3">
                     <div class="text-center my-2">
-                        <img src="http://localhost:5000/api/admin/partner/avatar/{{$partner['id']}}"
-                                class="rounded-circle" style="width: 60%" alt="Avatar" />
-                        {{-- @if ($partner['avatar'])
-                            
-                        @else
-                            <img src="{{ asset('assets/dashboard/img/dummyavatar.png') }}" class="rounded-circle"
-                                style="width: 60%" alt="Avatar" />
-                        @endif --}}
+                        <img src="http://localhost:5000/api/admin/partner/avatar/{{ $partner['id'] }}"
+                            class="rounded-circle" style="width:60px; height:60px; object-fit: cover;" alt="Avatar" />
                     </div>
                     <p class="my-1">
                         @if ($partner['account_status'] == 1)
