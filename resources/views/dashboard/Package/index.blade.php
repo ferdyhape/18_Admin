398827854d0facf4dfe35933d2b4a736f384ce52
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-20 13:05:13 +0700

    update input form on package view

--- resources/views/dashboard/Package/index.blade.php
+++ resources/views/dashboard/Package/index.blade.php
@@ -29,21 +29,21 @@
                                     <div class="modal-body">
                                         <div class="form-group">
                                             <input class="form-control mb-3 @error('package_name') is-invalid @enderror"
-                                                type="text" name="package_name" placeholder="package Name" required>
+                                                type="text" name="package_name" placeholder="Package Name" required>
                                             @error('package_name')
                                                 <div class="form-text">{{ $message }}</div>
                                             @enderror
                                         </div>
                                         <div class="form-group">
                                             <input class="form-control mb-3 @error('count_month') is-invalid @enderror"
-                                                type="number" name="count_month" placeholder="count month" required>
+                                                type="number" name="count_month" placeholder="Counf of Month" required>
                                             @error('count_month')
                                                 <div class="form-text">{{ $message }}</div>
                                             @enderror
                                         </div>
                                         <div class="form-group">
                                             <input class="form-control mb-3 @error('price') is-invalid @enderror"
-                                                type="price" name="price" placeholder="price" required>
+                                                type="price" name="price" placeholder="Price" required>
                                             @error('price')
                                                 <div class="form-text">{{ $message }}</div>
                                             @enderror
@@ -68,8 +68,8 @@
                         <thead>
                             <tr>
                                 <th>Package Name</th>
-                                <th>Jumlah Bulan</th>
-                                <th>Harga</th>
+                                <th>Counf of Month</th>
+                                <th>Price</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
@@ -126,19 +126,19 @@
                             <div class="form-group">
                                 <label for="editpackageName">Package Name</label>
                                 <input id="editpackageName" class="form-control @error('package_name') is-invalid @enderror"
-                                    type="text" name="package_name" placeholder="package Name">
+                                    type="text" name="package_name" placeholder="Package Name">
                                 @error('package_name')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
-                                <label for="editCMName">Count Month</label>
+                                <label for="editCMName">Counf of Month</label>
                                 <input id="editCMName" class="form-control @error('count_month') is-invalid @enderror"
-                                    type="text" name="count_month" placeholder="Count Month">
+                                    type="text" name="count_month" placeholder="Counf of Month">
                                 @error('count_month')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                                 <label for="editPriceName">Price</label>
                                 <input id="editPriceName" class="form-control @error('price') is-invalid @enderror"
-                                    type="text" name="price" placeholder="price">
+                                    type="text" name="price" placeholder="Price">
                                 @error('price')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
