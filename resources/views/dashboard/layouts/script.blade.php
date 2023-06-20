099e0e19459540c0fac3128ec61dd76dd8ee8e86
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-20 13:43:06 +0700

    comment unused script

--- resources/views/dashboard/layouts/script.blade.php
+++ resources/views/dashboard/layouts/script.blade.php
@@ -9,11 +9,11 @@
 <script src="{{ URL::asset('assets/dashboard/js/sb-admin-2.min.js') }}"></script>
 
 <!-- Page level plugins -->
-<script src="{{ URL::asset('assets/dashboard/vendor/chart.js/Chart.min.js') }}"></script>
+{{-- <script src="{{ URL::asset('assets/dashboard/vendor/chart.js/Chart.min.js') }}"></script> --}}
 
 <!-- Page level custom scripts -->
-<script src="{{ URL::asset('assets/dashboard/js/demo/chart-area-demo.js') }}"></script>
-<script src="{{ URL::asset('assets/dashboard/js/demo/chart-pie-demo.js') }}"></script>
+{{-- <script src="{{ URL::asset('assets/dashboard/js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ URL::asset('assets/dashboard/js/demo/chart-pie-demo.js') }}"></script> --}}
 <script src="{{ URL::asset('assets/dashboard/vendor/datatables/jquery.dataTables.min.js') }}"></script>
 <script src="{{ URL::asset('assets/dashboard/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
@@ -21,8 +21,8 @@
 <script src="{{ URL::asset('assets/dashboard/js/demo/datatables-demo.js') }}"></script>
 
 <!-- Page level custom scripts -->
-<script src="{{ URL::asset('assets/dashboard/js/demo/chart-area-demo.js') }}"></script>
+{{-- <script src="{{ URL::asset('assets/dashboard/js/demo/chart-area-demo.js') }}"></script>
 <script src="{{ URL::asset('assets/dashboard/js/demo/chart-pie-demo.js') }}"></script>
-<script src="{{ URL::asset('assets/dashboard/js/demo/chart-bar-demo.js') }}"></script>
+<script src="{{ URL::asset('assets/dashboard/js/demo/chart-bar-demo.js') }}"></script> --}}
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
